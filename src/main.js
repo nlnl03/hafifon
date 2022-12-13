e490bcd367f539d81e736a1f4b614bacedc51ce3
גדוד 383/לשכה/ע. קצין טכני/נועם לביא <s9053078@army.idf.il>
2022-12-13 13:34:26 +0200

    new

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import{createVuetify} from 'vuetify'
+// import   {Valert} from 'vuetify/lib/directives'
+
 
 createApp(App).use(router).mount('#app')
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,18 +1,28 @@
 <template>
 <div class="main">
+  <div class="right-side-flex"> 
   <div class="personal-info">
+      <div class="inner-right-flex"> 
         <div class="name-detail">
-            שם:   
-            מחלקה:  
+            <h2>נעם לביא</h2>  
+            <h5>מרתף הקסמים</h5>
+              
         </div>
+        <div class="hafifa-nickname"> 
 
+          </div>
+          </div>
+  <div class="inner-left-flex">
+    <img src=""> 
+  </div>
   </div>
   <div class="grades-info">
-        <div class="name-detail">
+        <!-- <div class="name-detail">
             שם:   
             מחלקה: 
-        </div>
+        </div> -->
  </div>
+ </div> 
    </div>
    <div class="drer">
      <vue-ellipse-progress />
@@ -38,22 +48,32 @@ export default {
 }
 </script>
 
-<style>
-.main{
-  display: flex;
-  flex-direction: column;
-  direction: rtl;
-     margin-right: 70px;
+<style scoped>
 
-  
+h5{
+  color:grey;
+}
+.right-side-flex{
+    width: 50%;
+    height: 80%;
+    display: flex;
+    margin-top: 30px;
+    flex-direction: column;
+    justify-content:flex-start;
+}
+.main{
+  /* display: flex; */
+  /* flex-direction: column; */
+  height: 95vh;
+  direction: rtl;
+  margin-right: 70px;
 }
 .personal-info{
-   background-color: #fff  ;
-   transform: translateY(10%);
+   /* background-color: #fff  ; */
    width: 32vw;
    height: 250px;
    border-radius: 20px 20px 20px 20px;
-   box-shadow: 0px 0px 100px 0px rgba(0,  0,  0,  0.2);
+   /* box-shadow: 0px 0px 100px 0px rgba(0,  0,  0,  0.2); */
    margin-bottom: 80px 
 }
  .grades-info{
@@ -64,12 +84,14 @@ export default {
    box-shadow: 0px 0px 100px 0px rgba(0,  0,  0,  0.2);
  }
 .name-detail{
-   padding:  0px 20px;
-   top: 40px;
-   margin-bottom: 70px ;
+   padding:  0px 10px;
+   /* margin-top: 40px;
+   margin-right: 40px;
+   margin-bottom: 70px ; */
    position: relative;
    font-size: 25px;
    display: flex;
+   border-right: 2px solid var(--main-background-color);
    flex-direction: column;
    text-align: right;
  }
